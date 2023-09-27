ead663f6d8532b21a84889ed4f19665252aae8e3
makenasandra <makenawachira@gmail.com>
2023-09-27 20:41:03 +0300

    renamed class

--- src/main/java/ticketing_system/app/Business/servises/UserService.java
+++ src/main/java/ticketing_system/app/Business/servises/UserService.java
@@ -1,9 +1,4 @@
 package ticketing_system.app.Business.servises;
 
-import ticketing_system.app.preesentation.data.UserDTO;
-
-public interface User_service {
-
-    UserDTO createUser(UserDTO userDTO);
-    void deleteUserById(long id);
+public interface UserService {
 }
