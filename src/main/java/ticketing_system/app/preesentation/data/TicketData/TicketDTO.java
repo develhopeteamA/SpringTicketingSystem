89a5100d288889e319789f86b32ff03ac1845149
makenasandra <makenawachira@gmail.com>
2023-10-07 19:57:34 +0300

    Added ticket controller

--- src/main/java/ticketing_system/app/preesentation/data/TicketData/TicketDTO.java
+++ src/main/java/ticketing_system/app/preesentation/data/TicketData/TicketDTO.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 import lombok.Setter;
 import ticketing_system.app.percistance.Entities.TicketEntities.Task;
 import ticketing_system.app.percistance.Entities.User;
+import ticketing_system.app.percistance.Enums.TicketPriorityLevel;
 import ticketing_system.app.percistance.Enums.TicketStatus;
 
+import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.List;
 @Getter
@@ -18,7 +20,7 @@ public class TicketDTO {
     private String description;
     private TicketStatus ticketStatus; //statusID from table or enum
     private String tags;
-    private String priorityLevel; //use Enum
+    private TicketPriorityLevel priorityLevel; //use Enum
     private User agentAssigned; //userID from User table
     private List<Task> subTasks;
     private String attachments;
@@ -27,4 +29,14 @@ public class TicketDTO {
     private Long createdBy;
     private Long updatedBy;
     private Timestamp updatedOn;
+
+    public TicketDTO(String ticketName, String description, TicketStatus ticketStatus, TicketPriorityLevel priorityLevel, String deadlineDate){
+        this.ticketStatus = ticketStatus;
+        this.ticketName = ticketName;
+        this.description = description;
+        this.priorityLevel = priorityLevel;
+        this.deadlineDate = Timestamp.valueOf(deadlineDate);
+    }
+
+    public TicketDTO(){}
 }
---
+++ src/test/java/ticketing_system/app/Business/implementation/TicketImplementation/TicketServiceImplementationTest.java
@@ -0,0 +1,34 @@
+package ticketing_system.app.Business.implementation.TicketImplementation;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketServiceImplementationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createTicket() {
+    }
+
+    @Test
+    void getTicket() {
+    }
+
+    @Test
+    void updateTicket() {
+    }
+
+    @Test
+    void deleteTicket() {
+    }
+}
