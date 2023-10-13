31d46f2584a007ca284995329bfdaac304921ecc
Maron <murangirimaron@gmail.com>
2023-10-13 20:17:01 +0300

    Update on Base files for user domain

--- src/main/java/ticketing_system/app/percistance/repositories/userRepositories/Userrepository.java
+++
@@ -1,9 +0,0 @@
-package ticketing_system.app.percistance.repositories.userRepositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ticketing_system.app.percistance.Entities.userEntities.Users;
-
-@Repository
-public interface Userrepository extends JpaRepository<Users, Long> {
-}
